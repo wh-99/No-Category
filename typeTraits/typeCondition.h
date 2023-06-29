659e75d38c6f00c0bad9e29dd2c1fc482ad96407
wh-99 <68727219+wh-99@users.noreply.github.com>
2023-06-30 00:20:33 +0900

    added enable_if

--- typeTraits/typeCondition.h
+++ typeTraits/typeCondition.h
@@ -1,5 +1,8 @@
 #pragma once
 
 namespace conditionBase {
-    
+    template <bool, typename = void> struct enableIF          { };
+    template <typename T>            struct enableIF<true, T> { using type = T; };
 }
+
+template <bool Condition, typename T = void> using enableIF = typename conditionBase::enableIF<Condition, T>::type;
